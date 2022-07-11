6273d51f58bdc05846b0baa53480ddb2edb5292a
Justin Knueppel <justinknueppel@gmail.com>
2022-07-10 23:50:20 -0400

    Fix rename script

--- rename.py
+++ rename.py
@@ -10,7 +10,7 @@ def parse_args():
                         help="input file, --format to see required format")
     parser.add_argument("--base-dir", "-b", dest="base_dir", default=".",
                         type=str, help="base directory to chdir into before renaming files")
-    parser.add_argument("--dry-run", "-d", dest="dry_run",
+    parser.add_argument("--dry-run", "-d", action="store_true", dest="dry_run",
                         help="check input files exist and print files to be renamed")
     parser.add_argument("--format", action="store_true",
                         help="print input file format")
@@ -46,7 +46,8 @@ if __name__ == "__main__":
     os.chdir(args.base_dir)
 
     if args.dry_run:
-        do_not_exist = list(filter(lambda e: not os.path.exists(e["old"])))
+        do_not_exist = list(
+            filter(lambda e: not os.path.exists(e["old"]), files))
         if len(do_not_exist) > 0:
             print("The following files were not found:")
             print("\n".join(map(lambda f: f["old"], files)))
